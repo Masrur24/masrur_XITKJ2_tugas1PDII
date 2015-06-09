4f7dcb1d1b5cda242233d6b560828a8cfc5fb7dc
masrur24 <masrurnazila8@gmail.com>
2015-06-09 10:16:39 +0700

    tugas 1

---
+++ unit1.pas
@@ -0,0 +1,40 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage(Edit1.text);
+  //jalankanprogram
+end;
+
+end.
+
